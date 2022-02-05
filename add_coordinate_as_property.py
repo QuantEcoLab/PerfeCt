bff07887f9445e9fa3c981b92d50dd76124af7ee
Tamara Đerđ <tamara.baji@gmail.com>
2022-02-05 18:21:41 +0100

    updated MED_farms database (removed aquacultures with coordintes (0,0))

--- add_coordinate_as_property.py
+++ add_coordinate_as_property.py
@@ -1,7 +1,7 @@
 from geojson import FeatureCollection
 import json
 
-path = 'data/MED_farms.geojson'
+path = 'data/MED_farms/MED_farms.geojson'
 
 with open(path,'r') as data_file:
     data = json.load(data_file)
@@ -16,5 +16,5 @@ for feature in feature_collection["features"]:
     feature["properties"]["coordinates"] = feature["geometry"]["coordinates"]
 
 json_string = json.dumps(feature_collection)
-with open('data/MED_farms_for_display.geojson', 'w') as outfile:
+with open('data/MED_farms/MED_farms_for_display.geojson', 'w') as outfile:
     outfile.write(json_string)
