457b3d52189156e313d448cb26930e3b5051bc65
Domagoj Hackenberger <domagojhack@gmail.com>
2022-02-05 15:51:06 +0100

    changes to vibrio simulation function naming

--- vibrio/vibrio.py
+++ vibrio/vibrio.py
@@ -9,14 +9,14 @@ N0 = 1  # Inicialna konc.
 r = 0.1  # Brzina rasta (log CFU/mL/h)
 K = 4  # Carrying capacity (log CFU/mL)
 
-T = 313.15 # dnevna temperatura
+T = 313.15  # dnevna temperatura
 
 
 def dNdt(t, N, T):
     return (r * N * (1 - (N / K)) * np.exp(TA/Tref - TA/T))
 
 
-def simulate(temp):
+def simulate_vibrio(temp):
     t_eval = np.arange(1, 24, 1/128)
     N = solve_ivp(dNdt,  [1, 24], [N0], args=[temp], t_eval=t_eval)
     return t_eval, N["y"][0]
@@ -24,6 +24,6 @@ def simulate(temp):
 
 if __name__ == "__main__":
     for i in [293.15, 298.15, 303.15, 308.15, 313.15]:
-        t, y = simulate(i)
+        t, y = simulate_vibrio(i)
 
         plt.plot(t, y)
