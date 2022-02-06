cbc3d9539ed3e4247b9413ae41bdfb978019e0d2
Domagoj Hackenberger <domagojhack@gmail.com>
2022-02-06 14:22:09 +0100

    started to work on new vibrio updated gitignore added deb_species_list table

---
+++ marketWeight.py
@@ -0,0 +1,8 @@
+from deb.simulations import simulate_deb, aux
+import numpy as np
+import pandas as pd
+
+
+data = pd.read_csv("data/time_series/id_638.csv", index_col=0)
+
+species = pd.read_csv("data/deb_species_list.csv")
---
+++ vibrio/vibrio_old.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+
+
+def dNdt(t, N, T):
+
+    Tref = 293.15  # Referentna temp
+    TA = 8000  # Arrhenius temp
+    N0 = 1  # Inicialna konc.
+    r = 0.1  # Brzina rasta (log CFU/mL/h)
+    K = 4  # Carrying capacity (log CFU/mL)
+
+    T = 313.15  # dnevna temperatura
+
+    return (r * N * (1 - (N / K)) * np.exp(TA/Tref - TA/T))
+
+
+def simulate_vibrio(temp):
+    t_eval = np.arange(1, 24, 1/128)
+    N = solve_ivp(dNdt,  [1, 24], [N0], args=[temp], t_eval=t_eval)
+    return t_eval, N["y"][0]
+
+
+if __name__ == "__main__":
+    for i in [293.15, 298.15, 303.15, 308.15, 313.15]:
+        t, y = simulate_vibrio(i)
+
+        plt.plot(t, y)
